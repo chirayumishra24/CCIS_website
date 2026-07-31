d3d6f3d677850ab24499824afb2f5824283f6b4a
Chirayu Mishra <chirayumishra24@gmail.com>
2026-07-31 15:24:46 +0530

    Add 4th student testimony video

--- src/app/api/admin/testimonials/route.ts
+++ src/app/api/admin/testimonials/route.ts
@@ -11,7 +11,8 @@ const defaultTestimonials = {
   student: [
     { id: 's1', img: 'student1.webp', videoId: 'd66JSRy8GwE' },
     { id: 's2', img: 'student2.webp', videoId: 'XWpU8A4BoHE' },
-    { id: 's3', img: 'student3.jpg', videoId: 'G5f7788rAbg' }
+    { id: 's3', img: 'student3.jpg', videoId: 'G5f7788rAbg' },
+    { id: 's4', img: 'student4.jpg', videoId: 'CkP3EudkpRQ' }
   ]
 };
 
